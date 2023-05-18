254824b248baf492dc9b802359f73bb5febc14da
Juan Silvestri <silvestrijuanignacio@gmail.com>
2023-05-18 09:12:03 -0300

    nuevos archivos

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char** argv) {
     std::vector<std::string> n_sizes = {"xl", "large", "medium", "small"};
 
-    for(int i=0; i<10; i++) {
-        for (int j = 0; j < 4; j++)
+    for (int j = 0; j < 4; j++)
+        for(int i=0; i<10; i++) {
         {
             std::string filename = "input/" + n_sizes[j] + "_" + std::to_string(i) + ".csv";
             std::cout << filename << std::endl;
